868131fe26336e51c3223b629a441e15a34f7edb
Steve Kang <iam@juneyoung.io>
2022-04-12 19:44:23 +0900

    fix(const): rename input exception and fix wrong print format

--- src/main/java/baseball/constant/InputExceptionMessage.java
+++ src/main/java/baseball/constant/InputExceptionMessage.java
@@ -1,6 +1,6 @@
 package baseball.constant;
 
-public class PlayerInputExceptionMessage {
+public class InputExceptionMessage {
     public static final String OUT_OF_LENGTH_RANGE = "입력이 허용된 길이 범위에 맞지 않습니다.";
     public static final String DUPLICATED_NUMBERS_PROVIDED = "중복된 숫자가 포함되어 있습니다.";
     public static final String EMPTY_NUMBER_PROVIDED = "입력은 비어있을 수 없습니다.";
--- src/main/java/baseball/constant/JudgementMessage.java
+++ src/main/java/baseball/constant/JudgementMessage.java
@@ -5,7 +5,7 @@ public class JudgementMessage {
 
     public static final String ONLY_STRIKE = "%s스트라이크";
     public static final String ONLY_BALL = "%s볼";
-    public static final String STRIKE_AND_BALL = "%s스트라이크 %s볼";
+    public static final String STRIKE_AND_BALL = "%s볼 %s스트라이크";
 
     public static final String NOTHING = "낫싱";
 }
